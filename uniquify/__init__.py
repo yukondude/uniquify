91b238691ddace5419fdcfc86c5f28bc65a374e8
Dave Rogers <thedude@yukondude.com>
2021-09-24 20:29:49 -0700

    Add GPL header.

---
+++ uniquify/__init__.py
@@ -0,0 +1,4 @@
+# This file is part of uniquify.
+# Copyright 2021 Dave Rogers <thedude@yukondude.com>.
+# Licensed under the GNU General Public License, version 3.
+# Refer to the attached LICENSE file or see <http://www.gnu.org/licenses/> for details.
